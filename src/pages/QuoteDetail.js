0aa365680278863f82b6430b684d09ea2cf61042
Bishop Bemby <bishopbemby1995@gmail.com>
2021-05-06 16:21:33 +0530

    nested routing for comments section

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,8 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+import { Link} from 'react-router-dom';
 const DUMMY_QUOTES = [
     {id: "q1", author: 'Bishop', text:'Learning React is fun'},
     {id: "q2", author: 'Bishu', text:'Learning React is great'},
@@ -18,7 +20,14 @@ if(!quote){
 
   return (
     <>
-      <HighlightedQuote author={quote.author} text={quote.text}/>
+      <HighlightedQuote author={quote.author} text={quote.text} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments...
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
